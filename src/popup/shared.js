59f06c8e600f9897f00a11f43058ea830c4c6955
Marcin <marcin@opensoftware.pl>
2020-05-23 14:06:07 +0200

    Update getToken function

--- src/popup/shared.js
+++ src/popup/shared.js
@@ -2,10 +2,9 @@ import axios from "axios"
 
 export const url = "https://api.nozbe.com:3000"
 
-export const getToken = () => {
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.get(['token'], r => { resolve(r.token) })
-  })
+export const getToken = async () => {
+  const result = await browser.storage.sync.get(['token'])
+  return result.token
 }
 
 export const calculateNextActionsNumber = async () => {
